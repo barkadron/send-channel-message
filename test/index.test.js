7688b9b5e0271b637f71478eac05f4b10b7f8d84
Andrey Barkalov <barkadron@mail.ru>
2018-12-28 23:14:12 +0300

    Add more tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,19 +1,41 @@
 import chai from 'chai';
-// import { mocks } from 'mock-browser';
 
 import sendChannelMessage from '../src/index';
 
+
+/* MessageChannel mock */
+global.port1 = {
+  close: () => null,
+};
+
+global.port2 = {
+  // "send" request message back
+  sendResponse: (message, origin) => global.port1.onmessage({ data: message, origin }),
+};
+
+global.MessageChannel = () => ({
+  port1: global.port1,
+  port2: global.port2,
+});
+
+/* target mock */
+const target = {
+  postMessage: (message, origin, [port2]) => port2.sendResponse(message, origin),
+};
+
+const testMessage = 'test message';
+
+
+/* test cases */
 describe('sendChannelMessage', () => {
-  it('must be a function', () => {
-    chai.assert.isFunction(sendChannelMessage);
-  });
-/*
-  @todo:
-  // https://wietse.loves.engineering/testing-promises-with-mocha-90df8b7d2e35
-  it('must return a promise', async () => {
-    const window = new mocks.MockBrowser().getWindow();
-    const result = await sendChannelMessage({ message: 'hello!', target: window });
-    chai.expect(result).to.equal('promise resolved');
-  });
-*/
+  it('must be a function.', () => chai.assert.isFunction(sendChannelMessage));
+
+  it('must rejects if input \'target\' is wrong.', () => sendChannelMessage(testMessage, null)
+    .catch(error => chai.assert.equal(error.message, 'Error! Invalid target.')));
+
+  it('must rejects if input \'message\' is wrong.', () => sendChannelMessage(null, target)
+    .catch(error => chai.assert.equal(error.message, 'Error! Invalid message.')));
+
+  it('must resolves and get response message.', () => sendChannelMessage(testMessage, target)
+    .then(result => chai.assert.equal(result, testMessage)));
 });
